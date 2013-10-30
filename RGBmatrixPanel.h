b85759db290de5592d8a17d223471fa7a1d545a8
protonmaster <protonmaster@gmail.com>
2013-10-30 22:30:37 +1300

    Update RGBmatrixPanel.h

--- RGBmatrixPanel.h
+++ RGBmatrixPanel.h
@@ -12,11 +12,11 @@ class RGBmatrixPanel : public Adafruit_GFX {
 
   // Constructor for 16x32 panel:
   RGBmatrixPanel(uint8_t a, uint8_t b, uint8_t c,
-    uint8_t sclk, uint8_t latch, uint8_t oe, boolean dbuf);
+    uint8_t sclk, uint8_t latch, uint8_t oe, boolean dbuf, uint8_t pwidth);
 
   // Constructor for 32x32 panel (adds 'd' pin):
   RGBmatrixPanel(uint8_t a, uint8_t b, uint8_t c, uint8_t d,
-    uint8_t sclk, uint8_t latch, uint8_t oe, boolean dbuf);
+    uint8_t sclk, uint8_t latch, uint8_t oe, boolean dbuf,uint8_t pwidth);
 
   void
     begin(void),
@@ -38,12 +38,12 @@ class RGBmatrixPanel : public Adafruit_GFX {
  private:
 
   uint8_t *matrixbuff[2];
-  uint8_t nRows, nPlanes, backindex;
+  uint8_t nRows, nPlanes, backindex, nPanels;
   boolean swapflag;
 
   // Init/alloc code common to both constructors:
   void init(uint8_t rows, uint8_t a, uint8_t b, uint8_t c,
-    uint8_t sclk, uint8_t latch, uint8_t oe, boolean dbuf);
+    uint8_t sclk, uint8_t latch, uint8_t oe, boolean dbuf, uint8_t pwidth);
 
   // PORT register pointers, pin bitmasks, pin numbers:
   volatile uint8_t
