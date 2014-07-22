b6b6150290bfe98e5abb7c2b76a1f6b7389d3b4b
Aerospacesmith <keanan_smith@hotmail.com>
2014-07-21 23:02:34 -0500

    Update RGBmatrixPanel.h
    Added Color method to pack r,g,b values into a single unit32_t value

--- RGBmatrixPanel.h
+++ RGBmatrixPanel.h
@@ -27,6 +27,8 @@ class RGBmatrixPanel : public Adafruit_GFX {
     dumpMatrix(void);
   uint8_t
     *backBuffer(void);
+  static uint32_t
+    Color(uint8_t r, uint8_t g, uint8_t b);
   uint16_t
     Color333(uint8_t r, uint8_t g, uint8_t b),
 	Color333(uint32_t c),
